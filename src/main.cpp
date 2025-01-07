9aba48a3ade132915c537494a281b8388d9f3f2b
Artturi Kerminen <kerminenartturi@gmail.com>
2025-01-07 17:00:00 +0200

    Add basic IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
-    const char *DATA_LOCATION = "../data";
+    const char *DATA_LOCATION = "data/db.txt";
 
     while (true)
     {
@@ -19,6 +21,36 @@ int main(int argc, char *argv[])
         {
             printf("%s\n", "Your input is wrong.");
             exit(1);
+        }
+
+        // Save a password to the database
+        if (input == "2")
+        {
+            printf("%s", "TEDSADSA");
+            std::string pass;
+
+            printf("%s\n", "Enter the password you want to save.");
+            getline(std::cin, pass);
+
+            std::string user;
+
+            printf("%s\n", "Enter the username for which you want to store the password.");
+            getline(std::cin, user);
+
+            if (std::filesystem::exists(DATA_LOCATION))
+            {
+                std::ofstream FILE(DATA_LOCATION, std::ios_base::app);
+
+                FILE << user << " " << pass << "\n";
+                FILE.close();
+            }
+            else
+            {
+                std::ofstream FILE(DATA_LOCATION);
+
+                FILE << user << " " << pass << "\n";
+                FILE.close();
+            }
         }
     }
 
